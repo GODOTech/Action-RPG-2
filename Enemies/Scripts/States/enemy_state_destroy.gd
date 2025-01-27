3629a07d128223e85e0f508c498f81bdb4ef1647
GODOTech <Prattz440@gmail.com>
2025-01-26 21:59:52 -0300

    Enemy drops implemented

--- Enemies/Scripts/States/enemy_state_destroy.gd
+++ Enemies/Scripts/States/enemy_state_destroy.gd
@@ -1,15 +1,22 @@
 class_name EnemyStateDestroy extends EnemyState
 
+const PICKUP = preload("res://Items/Item_Pickup/Item_Pickup.tscn")
+
 @export var anim_name : String = 'destroy'
 @export var knockback_speed : float = 200.0
 @export var decelerate_speed : float = 10.0
 
+
 @export_category('AI')
+
+@export_category("Item Drops")
+@export var drops : Array[ DropData ]
 
 @onready var hurt_box: HurtBox = $"../../HurtBox"
 
 var _damage_position : Vector2
 var _direction : Vector2
+
 
 
 func init() -> void:
@@ -27,6 +34,7 @@ func Enter() -> void:
 	enemy.velocity = _direction * -knockback_speed
 	enemy.UpdateAnimation( anim_name )
 	enemy.animation_player.animation_finished.connect( _on_animation_finished )
+	drop_items()
 	pass
 
 func Exit() -> void:
@@ -48,8 +56,27 @@ func _on_enemy_destroyed(hurt_box2 : HurtBox) -> void: # WARNING the 2 is to pre
 func _on_animation_finished( _a : String ) -> void:
 	enemy.queue_free()
 
+# Unused
+#func disble_hurt_box() -> void:
+	#var hurt_box : HurtBox = enemy.get_node_or_null("HurtBox")
+	#if hurt_box:
+		#hurt_box.monitoring = false
 
-
+func drop_items() -> void:
+	if drops.size() == 0 :
+		return
+	
+	for i in drops.size():
+		if drops[i] == null or drops [ i ].item == null :
+			continue
+		var drop_count : int = drops[ i ].get_drop_count()
+		for j in drop_count:
+			var drop = PICKUP.instantiate() as ItemPickup
+			drop.item_data = drops [ i ].item
+			enemy.get_parent().call_deferred( "add_child", drop )
+			drop.global_position = enemy.global_position
+			drop.velocity = enemy.velocity.rotated( randf_range( - 1.5, 1.5 ) ) * randf_range( 0.9, 1.5 )
+	pass
 
 
 
