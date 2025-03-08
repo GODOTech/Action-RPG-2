42a50b89a86dd48ff5b34210dddc8f11bab9a072
GODOTech <Prattz440@gmail.com>
2025-03-08 16:33:48 -0300

    Ambience, music and Binaural files implemented

---
+++ Audio/global_audio_manager.gd
@@ -0,0 +1,19 @@
+class_name GlobalAudio extends Node
+
+# remember to make it pausable from the menu
+
+@onready var binaural_player: AudioStreamPlayer = $BinauralPlayer
+@onready var ambience_player: AudioStreamPlayer = $AmbiencePlayer
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Audio Initialized")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
